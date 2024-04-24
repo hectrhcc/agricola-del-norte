49a12cfb9a5f3cf4a6c62995d98284a4c8d9757f
Hector <hcontrerascorvacho@gmail.com>
2024-04-23 21:06:49 -0400

    primera confirmación

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => { //para que funcionen los alert
+    const imagenes = document.querySelectorAll('.img-galeria')
+    const modal = document.querySelector('.modal');
+    const modalImg = document.querySelector('.modal-content');
+    const cerrar = document.querySelector('.close')
+
+    
+    imagenes.forEach(imagen =>{
+        imagen.addEventListener('click', ()=>{
+            modal.style.display = 'block'; 
+            modalImg.src = imagen.src; 
+            imagen.style.zIndex = '10';
+            modal.style.zIndex= '11';
+            document.body.classList.add('modal-open'); 
+        })
+    })
+    
+  
+    cerrar.addEventListener('click', () => {
+        modal.style.display = 'none'; 
+        document.body.classList.remove('modal-open'); 
+    });
+
+    var logo = document.querySelector('.logo');
+    logo.style.animation = 'emergeLogo 2s forwards'; // Cambia '1s' a la duración deseada de la animación
+});
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // Ruta a tu archivo HTML en el directorio raíz
+    "./src/**/*.js", // Ejemplo de otras rutas para archivos JavaScript
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
